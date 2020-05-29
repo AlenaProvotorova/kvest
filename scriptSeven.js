fbfe8105f9766b36a26988c3375df844ae29429b
AlenaProvotorova <lunichkina.alena@gmail.com>
2020-05-29 15:46:19 +0300

    Add folder img

--- scriptSeven.js
+++ scriptSeven.js
@@ -1,5 +1,5 @@
 const buttonCheckSeven = document.querySelector(".button-check7");
 
 buttonCheckSeven.addEventListener("click", () => {
-  window.location.href = "index8.html";
+  window.location = "index8.html";
 });
